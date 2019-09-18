d0f1bf4ee116be6ae1ebb5895686e14b18ec153f
Jacek Olszowski <jacek.olszowski1@gmail.com>
2019-09-18 21:03:24 +0200

    sending order to API

--- src/js/script.js
+++ src/js/script.js
@@ -383,11 +383,21 @@
             thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
             thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
 
+            thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+            thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
+            thisCart.dom.address = thisCart.dom.wrapper.querySelector(select.cart.address);
+
             thisCart.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
 
             for (let key of thisCart.renderTotalsKeys) {
                 thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
             }
+
+            thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+
+
+
+
 
         }
         initActions() {
@@ -402,6 +412,51 @@
             thisCart.dom.productList.addEventListener('remove', function () {
                 thisCart.remove(event.detail.cartProduct);
             });
+
+            thisCart.dom.form.addEventListener('submit', function () {
+                event.preventDefault();
+                thisCart.sendOrder();
+            })
+
+        }
+
+        sendOrder() {
+            const thisCart = this;
+            const url = settings.db.url + '/' + settings.db.order;
+
+            const payload = {
+                address: thisCart.dom.address,
+                phone: thisCart.dom.phone,
+                totalNumber: thisCart.totalNumber,
+                subtotalPrice: thisCart.subtotalPrice,
+                totalPrice: thisCart.totalPrice,
+                deliveryFee: thisCart.deliveryFee,
+                products: [],
+
+            };
+
+            for (let product of thisCart.products) {
+                payload.products.push(product.getData());
+
+                console.log('product: ', product);
+            }
+
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+            };
+
+            fetch(url, options)
+                .then(function (response) {
+                    return response.json();
+                }).then(function (parsedResponse) {
+                    console.log('parsedResponse', parsedResponse);
+                });
+
+
 
         }
         add(menuProduct) {
@@ -465,6 +520,8 @@
 
 
         }
+
+
 
 
     }
@@ -547,6 +604,22 @@
 
             console.log('remove: ', thisCartProduct);
         }
+
+        getData() {
+            const thisCartProduct = this;
+
+            return {
+                id: this.id,
+                amount: this.amount,
+                price: this.price,
+                params: this.params,
+                priceSingle: this.priceSingle,
+                name: this.name,
+
+            };
+        }
+
+
     }
 
 
@@ -560,7 +633,7 @@
             //            console.log('thisApp.data: ', thisApp.data);
 
             for (let productData in thisApp.data.products) {
-                
+
                 new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
             }
         },
@@ -568,25 +641,25 @@
             const thisApp = this;
 
             thisApp.data = {};
-            
+
             const url = settings.db.url + '/' + settings.db.product;
-            
+
             fetch(url)
-                .then(function(rawResponse){
-                return rawResponse.json();
+                .then(function (rawResponse) {
+                    return rawResponse.json();
                 })
-                .then(function(parsedResponse){
-                console.log('parsedResponse: ', parsedResponse);
-                
-            
-                //save parsedResponse as thisApp.data.products //
-                thisApp.data.products = parsedResponse;
-                //execute initMenu method //
-                thisApp.initMenu();
+                .then(function (parsedResponse) {
+                    console.log('parsedResponse: ', parsedResponse);
+
+
+                    //save parsedResponse as thisApp.data.products //
+                    thisApp.data.products = parsedResponse;
+                    //execute initMenu method //
+                    thisApp.initMenu();
                 });
-            
+
             console.log('thisApp.data ', JSON.stringify(thisApp.data));
-            
+
         },
 
         initCart: function () {
@@ -604,7 +677,7 @@
             //            console.log('settings:', settings);
             //            console.log('templates:', templates);
             thisApp.initData();
-            
+
             thisApp.initCart();
         },
 
